fb64e2c3e2d6ffa7e62fc8e550cb18864750789f
Anubhav Ghosh <anubhavg8@gmail.com>
2017-07-19 11:52:24 +0530

    Update Registration.php

--- Registration.php
+++ Registration.php
@@ -10,15 +10,10 @@ $from = new SendGrid\Email(null, $email); //the person who fills the form
 $subject = $_POST['schoolname'];
 $to = new SendGrid\Email(null, "info@bispoetryslam.in"); // defaut email , no changes required 
 $content1 = new SendGrid\Content("text/plain", $msg);
-
-
 // 
 
 
-
 $mail = new SendGrid\Mail($from, $subject, $to, $content1, $content1);
-
-$apiKey = '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';
 $sg = new \SendGrid($apiKey);
 
 $response = $sg->client->mail()->send()->post($mail);
@@ -27,8 +22,4 @@ var_dump($response->headers());
 // echo $response->body();
 
 header( "Location:contactMessage.html" );
-// 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
-
-
-//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
 ?>
