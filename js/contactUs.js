d2dbd2db27ee8ebab4c60048bc96f45edfa7ea8f
Anubhav Ghosh <anubhavg8@gmail.com>
2017-07-19 11:58:27 +0530

    Add files via upload

---
+++ js/contactUs.js
@@ -0,0 +1,11 @@
+function check(){
+
+    var name = document.getElementById("name").value;
+    var school = document.getElementById("name").value;
+    var email = document.getElementById("name").value;
+    var Msg = document.getElementById("name").value;
+    
+    $.ajax
+
+
+}
---
+++ js/main.js
@@ -0,0 +1,8 @@
+jquery on change 
+
+<a id=xxx>
+
+<a href="xxx">
+
+a tag link go same page 
+
